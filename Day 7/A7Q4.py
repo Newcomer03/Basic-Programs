8c4a630e1dcc58f92df6bb6fcd5f09d390d44103
Soumyadeep Das <71889686+Newcomer03@users.noreply.github.com>
2021-07-11 19:23:03 +0530

    Delete Day 7 directory

--- Day 7/A7Q4.py
+++
@@ -1,5 +0,0 @@
-def sort_hyphen(s):
-    l = s.split("-")
-    l.sort()
-    print('-'.join(l))
-sort_hyphen(input("Enter a sequence seperated by '-'\n"))
--- Day 7/A7Q7.py
+++
@@ -1,8 +0,0 @@
-def posSum(n):
-    if n<1:
-        return 0
-    else:
-        return n + posSum(n-2)
-    
-sum = posSum(int(input("Enter a Number:\n")))
-print(sum)
